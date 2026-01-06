48af5768e771e8f95b9df04a66992db50ae08e78
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-01-06 23:43:42 +0530

    Emailjs integration

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,9 +1,36 @@
 import React from "react";
 import Layout from "../components/Layout";
+import emailjs from "emailjs-com";
+import { ToastContainer, toast } from 'react-toastify'
+import "react-toastify/dist/ReactToastify.css"
 
 const Contact = () => {
+
+ const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        import.meta.env.VITE_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        e.target,
+        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          e.target.reset();
+          toast.success("Message sent successfully!!")
+        },
+        (error) => {
+          console.error(error);
+          toast.error("Failed to send Message. Please Try Again")
+        }
+      );
+  };
+
   return (
     <Layout>
+      <ToastContainer position="top-center" autoClose={3000} />
       <section
         className="min-h-screen px-4 py-16 bg-linear-to-br from-[#240750] via-[#344C64]  to-[#240750] text-white flex items-center"
       >
@@ -22,7 +49,7 @@ const Contact = () => {
               <div className="flex items-center gap-4 bg-white/5 p-4 rounded-lg backdrop-blur">
                 <span className="text-xl"><i className="fas fa-envelope"></i></span>
                 <span className="text-sm text-gray-200">
-                  ladgames@gmail.com
+                  ladgamesinfo@gmail.com
                 </span>
               </div>
 
@@ -49,12 +76,14 @@ const Contact = () => {
               Send us a message
             </h2>
 
-            <form className="space-y-5">
+            <form onSubmit={sendEmail} className="space-y-5">
 
               <div>
                 <label className="text-sm text-gray-300">Your Name</label>
                 <input
                   type="text"
+                  name="user_name"
+                  required
                   placeholder="John snow"
                   className="mt-1 w-full px-4 py-2.5 rounded-lg bg-[#344C64]/70 border border-[#577B8D] focus:outline-none focus:ring-2 focus:ring-[#57A6A1]
                     text-white"
@@ -65,6 +94,8 @@ const Contact = () => {
                 <label className="text-sm text-gray-300">Email Address</label>
                 <input
                   type="email"
+                  name="user_email"
+                  required
                   placeholder="John@email.com"
                   className="mt-1 w-full px-4 py-2.5 rounded-lg bg-[#344C64]/70 border border-[#577B8D] focus:outline-none focus:ring-2 focus:ring-[#57A6A1]
                     text-white"
@@ -75,6 +106,8 @@ const Contact = () => {
                 <label className="text-sm text-gray-300">Message</label>
                 <textarea
                   rows="4"
+                  name="message"
+                  required
                   placeholder="Write your message here..."
                   className="mt-1 w-full px-4 py-2.5 rounded-lg bg-[#344C64]/70 border border-[#577B8D] focus:outline-none  focus:ring-2                   focus:ring-[#57A6A1] text-white resize-none"
                 ></textarea>
